45d3cb50ce51bc4c630843b97e6efa411816203a
Jahnavi <jahnavi.b2@gmail.com>
2025-08-28 17:37:40 -0400

    Adding programs

--- src/mergeAlternatively.java
+++ src/mergeAlternatively.java
@@ -1,2 +1,11 @@
 public class mergeAlternatively {
+    public static String mergeAlternatively(String w1,String w2){
+
+    }
+    public static void main(String[] args){
+        String s1 = "Jh";
+        String s2 = "an";
+        String result = mergeAlternatively(s1,s2);
+        System.out.println("Merged strings is " + result);
+    }
 }
