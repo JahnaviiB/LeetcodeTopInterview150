2ffd598f9b9f317f05870382e256d3680d298dc8
Jahnavi <jahnavi.b2@gmail.com>
2025-09-02 13:08:59 -0400

    Adding programs

--- src/mergeAlternatively.java
+++ src/mergeAlternatively.java
@@ -1,12 +1,20 @@
 public class mergeAlternatively {
     public static String mergeAlternatively(String w1,String w2){
         StringBuilder merged = new StringBuilder();
-        int max_lenght = Math.max(w1.length(),w2.length());
-
+        int maxLength = Math.max(w1.length(),w2.length());
+        for(int i=0;i<maxLength;i++){
+            if(i<w1.length()){
+                merged.append(w1.charAt(i));
+            }
+           if(i<w2.length()){
+               merged.append(w2.charAt(i));
+           }
+        }
+        return merged.toString();
     }
     public static void main(String[] args){
-        String s1 = "Jh";
-        String s2 = "an";
+        String s1 = "Jha";
+        String s2 = "anvi";
         String result = mergeAlternatively(s1,s2);
         System.out.println("Merged strings is " + result);
     }
